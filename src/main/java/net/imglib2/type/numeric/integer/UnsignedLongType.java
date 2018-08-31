65d25f4fac84a578616b77b3bba954c23249eff1
tpietzsch <tobias.pietzsch@gmail.com>
2018-08-31 18:13:18 +0200

    UnsignedLongType doesn't have to override valueEquals, equals, hashCode

--- src/main/java/net/imglib2/type/numeric/integer/UnsignedLongType.java
+++ src/main/java/net/imglib2/type/numeric/integer/UnsignedLongType.java
@@ -11,7 +11,7 @@
  * %%
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  * 1. Redistributions of source code must retain the above copyright notice,
  *    this list of conditions and the following disclaimer.
  * 2. Redistributions in binary form must reproduce the above copyright notice,
@@ -346,23 +346,5 @@ public class UnsignedLongType extends GenericLongType< UnsignedLongType >
 	public int compareTo( final UnsignedLongType other )
 	{
 		return compare( get(), other.get() );
-	}
-
-	@Override
-	public boolean valueEquals( UnsignedLongType other )
-	{
-		return getLong() == other.getLong();
-	}
-
-	@Override
-	public boolean equals( final Object obj )
-	{
-		return Util.valueEqualsObject( this, obj );
-	}
-
-	@Override
-	public int hashCode()
-	{
-		return Long.hashCode( getLong() );
 	}
 }
